b85eb055dcd0e856cf9e6bcde975be9fd9a80e45
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-10-30 18:20:00 +1100

    Deploy test part 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist';
-//import tailwind
+//Testo
 
 import App from './App.jsx';
 import Login from './pages/Login.jsx';
